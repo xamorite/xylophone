29c14f77494522eaa0255f9b9c0b039f9a3b91d2
Xamorite <eogunneye@gmail.com>
2023-11-20 13:21:58 +0100

    Xylophone app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,10 +10,55 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(child:
-          TextButton(onPressed: (){}, child: Text('Click Me')
+          child: Center(
+            child: Container(
+              child: Column(
+                children: [
+                  TextButton(onPressed: (){
+                    final player = AudioCache();
+                    player.load('note1.wav');
+                  }, child: SizedBox(width: 250.0,)
 
-            ,),
+                    ,),
+                  TextButton(onPressed: (){
+                    final player = AudioCache();
+                    player.load('note2.wav');
+                  }, child: Text('Click Me')
+
+                    ,),
+                  TextButton(onPressed: (){
+                    final player = AudioCache();
+                    player.load('note3.wav');
+                  }, child: Text('Click Me')
+
+                    ,),
+                  TextButton(onPressed: (){
+                    final player = AudioCache();
+                    player.load('note4.wav');
+                  }, child: Text('Click Me')
+
+                    ,),
+                  TextButton(onPressed: (){
+                    final player = AudioCache();
+                    player.load('note5.wav');
+                  }, child: Text('Click Me')
+
+                    ,),
+                  TextButton(onPressed: (){
+                    final player = AudioCache();
+                    player.load('note6.wav');
+                  }, child: Text('Click Me')
+
+                    ,),
+                  TextButton(onPressed: (){
+                    final player = AudioCache();
+                    player.load('note7.wav');
+                  }, child: Text('Click Me'),
+
+                    ),
+                ],
+              ),
+            ),
           ),
     ),
       ),
