ca049aa00a67854eee254a47ea3e9e484364db29
Xamorite <eogunneye@gmail.com>
2023-11-20 22:26:22 +0100

    Xylophone app

--- lib/main.dart
+++ lib/main.dart
@@ -17,93 +17,90 @@ class XylophoneApp extends StatelessWidget {
         backgroundColor: Colors.black,
         body: SafeArea(
           child: Center(
-            child: Expanded(
-              // width: 250.0,
-              // height: 500.0,
-              child: Column(
-                children: [
-                  Expanded(
-                    child: TextButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-                      ),
-                      onPressed: (){
-                     playSound(1);
-
-                    }, child: SizedBox(width: 400.0,)
-
-                      ,),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
-                      ),
-                      onPressed: (){
-                        playSound(2);
-                    }, child: SizedBox(width: 400.0,)
-
-
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
                     ),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
-                      ),
-                      onPressed: (){
-                        playSound(3);
-                    }, child:SizedBox(width: 400.0,
-                    )
+                    onPressed: (){
+                   playSound(1);
 
-                      ,),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-                      ),
-                      onPressed: (){
-                        playSound(4);
-                    }, child: SizedBox(width: 400.0,)
+                  }, child: SizedBox(width: 400.0,)
 
-                      ,),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.indigo),
-                      ),
-                      onPressed: (){
-                        playSound(5);
-                    },
-                      child: SizedBox(width: 400.0,)
-                      ,),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                      ),
-                      onPressed: (){
-                        playSound(6);
-                    }, child: SizedBox(width: 400.0,)
+                    ,),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
+                    ),
+                    onPressed: (){
+                      playSound(2);
+                  }, child: SizedBox(width: 400.0,)
 
-                      ,),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.pink),
-                      ),
-                      onPressed: (){
-                        playSound(7);
-                    }, child: SizedBox(width: 400.0,)
 
-                     ),
                   ),
-                ],
-              ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
+                    ),
+                    onPressed: (){
+                      playSound(3);
+                  }, child:SizedBox(width: 400.0,
+                  )
+
+                    ,),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                    ),
+                    onPressed: (){
+                      playSound(4);
+                  }, child: SizedBox(width: 400.0,)
+
+                    ,),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.indigo),
+                    ),
+                    onPressed: (){
+                      playSound(5);
+                  },
+                    child: SizedBox(width: 400.0,)
+                    ,),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                    ),
+                    onPressed: (){
+                      playSound(6);
+                  }, child: SizedBox(width: 400.0,)
+
+                    ,),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.pink),
+                    ),
+                    onPressed: (){
+                      playSound(7);
+                  }, child: SizedBox(width: 400.0,)
+
+                   ),
+                ),
+              ],
             ),
           ),
     ),
